b5f6780ffde9c13068029837845714d25eb59188
Yogesh <balarajuyogesh@gmail.com>
2018-08-31 23:13:12 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
 
 // creating a Home route
 app.get('/', (req, res) => res.send("Welcome to GraphQL with express"));
 // Listen on port 8080
-app.listen(8080, () => console.log('graphQL app is listening on port 8080'));
+app.listen(PORT, HOST, () => console.log('graphQL app is listening on port 8080'));
